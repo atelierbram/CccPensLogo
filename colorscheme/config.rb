3c61870f3660bdf198436a79d8437f29583c9aa6
atelierbram <bramdeh@gmail.com>
2015-08-24 11:37:17 +0200

    cleaning up the Sass

--- colorscheme/config.rb
+++ colorscheme/config.rb
@@ -1,13 +1,21 @@
 # Require any additional compass plugins here.
-
-# https://github.com/ai/autoprefixer
+#
+# https://github.com/postcss/autoprefixer#Compass
 require 'autoprefixer-rails'
-# require 'csso'
 
 on_stylesheet_saved do |file|
   css = File.read(file)
-  File.open(file, 'w') do |io|
-    io << AutoprefixerRails.process(css)
+  map = file + '.map'
+
+  if File.exists? map
+    result = AutoprefixerRails.process(css,
+    from: file,
+    to:   file,
+    map:  { prev: File.read(map), inline: false })
+    File.open(file, 'w') { |io| io << result.css }
+    File.open(map,  'w') { |io| io << result.map }
+  else
+    File.open(file, 'w') { |io| io << AutoprefixerRails.process(css) }
   end
 end
 
@@ -16,23 +24,21 @@ css_dir = '/css'
 sass_dir = '/sass'
 images_dir = '/img'
 javascripts_dir = '/js'
-relative_assets = true
-# line_comments = true
-# output_style = :compressed
-preferred_syntax = :scss
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
 # preferred_syntax = :sass
+preferred_syntax = :scss
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
--- config.rb
+++ config.rb
@@ -1,13 +1,21 @@
 # Require any additional compass plugins here.
 #
-# https://github.com/ai/autoprefixer
+# https://github.com/postcss/autoprefixer#Compass
 require 'autoprefixer-rails'
-# require 'csso'
 
 on_stylesheet_saved do |file|
   css = File.read(file)
-  File.open(file, 'w') do |io|
-    io << AutoprefixerRails.process(css)
+  map = file + '.map'
+
+  if File.exists? map
+    result = AutoprefixerRails.process(css,
+    from: file,
+    to:   file,
+    map:  { prev: File.read(map), inline: false })
+    File.open(file, 'w') { |io| io << result.css }
+    File.open(map,  'w') { |io| io << result.map }
+  else
+    File.open(file, 'w') { |io| io << AutoprefixerRails.process(css) }
   end
 end
 
@@ -22,7 +30,7 @@ javascripts_dir = "assets/js"
 # output_style = :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
@@ -32,5 +40,6 @@ color_output = false
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
 # preferred_syntax = :sass
+preferred_syntax = :scss
 # and then run:
 # sass-convert -R --from scss --to sass css scss && rm -rf sass && mv scss sass
